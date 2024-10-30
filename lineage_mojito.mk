b3f9a0369a2f21ead1b6e04a5c5a1b0fd1de954c
Sachinpawar86 <sachinbarange86@gmail.com>
2024-10-30 14:58:02 +0000

    mojito : bringup for alphadroid and added some flags

--- lineage_mojito.mk
+++ lineage_mojito.mk
@@ -11,8 +11,47 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some LineageOS stuffs
+# Inherit some AlphaDroid stuffs
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# BOOT_ANIMATION
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device config
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Nuke AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
+# Blur effect
+TARGET_ENABLE_BLUR := true
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 3
+
+# TARGET_LAUNCHER options:
+# 1 - stock (default)
+# 2 - lawnchair
+# 3 - pixel (valid only on gapps builds)
+TARGET_LAUNCHER := 1
+
+# GAPPS (valid only for GAPPS builds)
+TARGET_SUPPORTS_QUICK_TAP := true
+TRGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+# TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := Unofficial
+ALPHA_MAINTAINER := Sachin_07
+
+# Debugging
+TARGET_INCUDE_MATLOG := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_mojito
